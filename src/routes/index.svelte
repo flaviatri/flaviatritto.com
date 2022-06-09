b2f191c61fa32c99d3675131d13ca57f4cf6c5f4
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-06-09 21:17:14 +0200

    rebuild with object

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,22 @@
+<script lang="ts">
+	import { assets } from '$app/paths';
+</script>
+
 <div>
-	<iframe
+	<object
+		data="{assets}/Tritto-Portfolio.pdf"
+		title="portfolio"
+		type="application/pdf"
+		width="100%"
+		height="100%"
+	/>
+	<!-- <iframe
 		title="portfolio"
 		src="https://drive.google.com/file/d/1ouFVaUXEiOjHZawUqbsYOIoJGQ28sHWS/preview"
 		width="100%"
 		height="100%"
 		allow="autoplay"
-	/>
+	/> -->
 </div>
 
 <style>
