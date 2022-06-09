43547486cce325f175414eec75aa8edc100dabb1
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2022-06-09 20:46:10 +0200

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<div>
+	<iframe
+		title="portfolio"
+		src="https://drive.google.com/file/d/1ouFVaUXEiOjHZawUqbsYOIoJGQ28sHWS/preview"
+		width="100%"
+		height="100%"
+		allow="autoplay"
+	/>
+</div>
+
+<style>
+	:global(body, html) {
+		margin: 0;
+		padding: 0;
+	}
+
+	div {
+		height: 100vh;
+		width: 100vw;
+	}
+</style>
