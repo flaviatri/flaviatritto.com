3a71b5815c3d5be723ed6eecf7f6909fe045c011
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-10-31 01:27:16 +0100

    Revert "cleanup (#1)"
    This reverts commit 8b6c159816a815a6f769ed66701e9ff6e7d8fa1a.

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<div>
+	<iframe
+		title="portfolio"
+		src="https://drive.google.com/file/d/1_KvACc1Y2VKYnH5GEv5C6u2B842Efyk3/preview"
+		width="100%"
+		height="100%"
+		allow="autoplay"
+	/>
+</div>
+
+<style>
+	:global(body, html) {
+		margin: 0;
+		padding: 0;
+	}
+
+	div {
+		height: 100vh;
+		width: 100vw;
+	}
+</style>
